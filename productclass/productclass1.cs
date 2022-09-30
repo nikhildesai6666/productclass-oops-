fbaf406b7884781cab8e6be3f65481c5fa544c4e
nikhildesai6666 <desainikhil9664@gmail.com>
2022-09-30 12:33:52 +0530

    Add project files.

---
+++ productclass/productclass1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace productclass
+{
+    internal class productclass1
+    {
+        static void Main(string[]args)
+        {
+            product p1 = new product();
+            p1.Dataadd(1, 40000, 10, "DELL");
+            Console.WriteLine(p1.Display());
+        }
+            
+    }
+}
